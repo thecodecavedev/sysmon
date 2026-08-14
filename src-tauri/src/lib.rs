5ac3ce34af357b0b5659214aa11d75be2a3c4db3
The Code Cave Dev <melusi.somkhanda@gmail.com>
2026-08-15 00:09:07 +0200

    ram updates every 2 seconds and progress bar added

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,8 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-use std::net::TcpStream;
 use std::collections::HashMap;
-use sysinfo::{
-    Components,Disks,Networks,System,
-};
+use std::{net::TcpStream, time::Duration};
+use sysinfo::System;
+use tauri::{AppHandle, Emitter};
 
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,30 +10,50 @@ fn greet(name: &str) -> String {
 }
 
 #[tauri::command]
-fn get_stats() -> (bool,HashMap<String,String>) {
+fn get_stats() -> (bool, HashMap<String, String>) {
     let mut sys = System::new_all();
-    sys.refresh_all(); 
+
+    sys.refresh_all();
     let mut stats: HashMap<String, String> = HashMap::new();
 
-    let mut is_connected = false;
+    let is_connected;
 
-    if let Ok(stream) = TcpStream::connect("209.85.233.101:80") {
+    if let Ok(_stream) = TcpStream::connect("209.85.233.101:80") {
         is_connected = true;
     } else {
         is_connected = false;
     }
 
-    stats.insert("CPU".to_string(),sys.cpus().len().to_string());
-    stats.insert("RAM".to_string(),sys.used_memory().to_string());
+    stats.insert("TOTAL_RAM".to_string(), sys.total_memory().to_string());
+    stats.insert("RAM".to_string(), sys.used_memory().to_string());
 
-    (is_connected,stats)
-    
+    (is_connected, stats)
+}
+
+// #[tauri::command]
+fn update_ram_stats(app: AppHandle) {
+    tauri::async_runtime::spawn(async move {
+        let mut sys = System::new_all();
+        loop {
+            sys.refresh_memory();
+            let ram_used: String = sys.used_memory().to_string();
+            if let Err(e) = app.emit("update_ram_stats", ram_used) {
+                eprintln!("failed to emit event: {:?}", e);
+            }
+
+            tokio::time::sleep(Duration::from_secs(2)).await;
+        }
+    });
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
+        .setup(|app| {
+            update_ram_stats(app.handle().clone());
+            Ok(())
+        })
         .invoke_handler(tauri::generate_handler![greet, get_stats])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src/main.js
+++ src/main.js
@@ -1,16 +1,30 @@
-
+// import { listen } from "@tauri-apps/api/event"
 
 const { invoke } = window.__TAURI__.core;
+const { listen, emit } = window.__TAURI__.event;
 
 let valu
 let connection_status_dot
 let refresh_btn
 let ram_val
 let cpu_val
+let ram
+let total_ram
+let ram_progress_bar
 
 async function greet() {
   // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
   greetMsgEl.textContent = await invoke("greet", { name: greetInputEl.value });
+}
+
+
+function init() {
+  listen("update_ram_stats", (e) => {
+    ram_val.textContent = Number.parseFloat(parseInt(e.payload) / 1000000000).toFixed(2) + " GB"
+    ram_progress_bar.value = (e.payload/total_ram)*100
+  })
+
+  console.log(ram_progress_bar)
 }
 
 async function get_stats() {
@@ -18,14 +32,14 @@ async function get_stats() {
   let stats = await invoke("get_stats", {})
 
   let is_connected = stats[0];
-  let ram = stats[1]["RAM"]
-  let cpu = stats[1]["CPU"]
-  console.log(await invoke("get_stats", {})[0])
+  ram = stats[1]["RAM"]
+  total_ram = stats[1]["TOTAL_RAM"]
+  // console.log(await invoke("get_stats", {})[0])
   connection_status_dot.style.cssText = is_connected ? `background-color:green` : `background-color:red`
   valu.textContent = is_connected ? "Online" : "Offline"
-  ram_val.textContent = parseInt(ram)/1000000+" MB"
-  cpu_val.textContent = cpu
-  
+  ram_val.textContent = Number.parseFloat(parseInt(ram) / 1000000000).toFixed(2) + " GB"
+  // cpu_val.textContent = cpu
+
 }
 
 
@@ -35,17 +49,16 @@ window.addEventListener("DOMContentLoaded", () => {
   ram_val = document.querySelector(".ram")
   cpu_val = document.querySelector(".cpu")
   let btn = document.getElementById("btn")
+  ram_progress_bar = document.getElementById("ram-prog")
   connection_status_dot = document.querySelector(".dot")
   refresh_btn = document.querySelector(".refresh")
   get_stats()
+  init()
 
   refresh_btn.addEventListener("click", (e) => {
     get_stats()
   })
 
-  setTimeout(() => {
-    console.log(valu)
-  }, 2000
-  )
 
 });
+
